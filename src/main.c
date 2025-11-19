12db66a0fffebf78386e91c88575e80767e53974
Lassi Åström <lastrom23@student.oulu.fi>
2025-11-19 15:51:44 +0200

    Korjattu syntaksia, lisätty globaali muuttuja output_buffer, yms.

--- src/main.c
+++ src/main.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <string.h>
 
@@ -10,16 +9,21 @@
 
 #include "tkjhat/sdk.h"
 
+//Kurssi reposta, ei mitään hajua tarvitaanko
+#define DEFAULT_STACK_SIZE 2048
+#define CDC_ITF_TX      1
 
-//Lisätty
+//Nämä on global variables, pitää varmaan lisätä vielä jotaki
+QueueHandle_t output_buffer;
+//Yllä oleva täältä
+//https://www.freertos.org/Documentation/02-Kernel/04-API-references/06-Queues/01-xQueueCreate
+char morse;
 
-char morse = '.';
 
 void init_sw1() {
     // Initialize the button pin as an input with a pull-up resistor
     gpio_init(SW1_PIN);
     gpio_set_dir(SW1_PIN, GPIO_IN);
-
 }
 
 void init_sw2() {
@@ -37,12 +41,12 @@ void init_button2(){
 }
 
 
-void imuTask(){
+void imuTask(void *p){
 
     float ax, ay, az;
 
-    while{
-        ICM42670_read_sensor_data (float &ax, float &ay, float &az, NULL, NULL, NULL, NULL);
+    while(1){
+        ICM42670_read_sensor_data (&ax, &ay, &az, NULL, NULL, NULL, NULL);
 
         if(az > 0.7f){
             morse = '.';
@@ -52,11 +56,29 @@ void imuTask(){
         }
 
     }
+
+    vTaskDelay(pdMS_TO_TICKS(1000));
 }
 
-void buttonTask(){
-
+void buttonTask(void *p){
+    /*
+    TBD
+    */
 }
+
+//Otettu kurssin reposta
+static void usbTask(void *arg) {
+    (void)arg;
+    while (1) {
+        tud_task();              // With FreeRTOS wait for events
+                                 // Do not add vTaskDelay. 
+    }
+}
+
+/*
+Tähän kait vielä esim. usbOutputTask yms. joka lähettää sen viestin
+*/
+
 
 int main(void) {
     stdio_init_all();
@@ -65,6 +87,14 @@ int main(void) {
 
     init_ICM42670();                        // IMU WHO_AM_I + basic setup
     ICM42670_startAccel(100, 4);            // 100 Hz, ±4 g
+
+    init_button1;
+    init_button2;
+    output_buffer = xQueueCreate(32, sizeof(char));
+
+    /*
+    Täällä ne pitäs sitte luoda ne taskit xTaskCreate
+    */
 
 }
 
